3b58280654bc38a3af1f36d9c3baa84efbb8d01b
urzyasar <48052872+urzyasar@users.noreply.github.com>
2019-03-20 16:07:49 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
    int num;
    printf("enter an integer");
    scanf("%d",&num);
+   if(num>=0)
+   {
    if (num%2==0)
    printf("%d is even number",num);
    else
    printf("%d is odd number",num);
-   return 0;
+   }
+   else
+   printf("%d is invalid input",num);
+   
 }
